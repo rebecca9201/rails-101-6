b7db2652bb1847332ae721665154045d19b4c049
Rebecca Wang <wangmmqq@gmail.com>
2016-07-30 21:01:14 +0800

    define find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -23,11 +24,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Successfully."
     else
@@ -36,13 +35,19 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "The group has been deleted successfully."
   end
 
 
 private
+
+   def find_group_and_check_permission
+     @group = Group.find(params[:id])
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission".
+     end
+   end
 
    def group_params
      params.require(:group).permit(:title, :description)
