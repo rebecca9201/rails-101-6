9a0fd83d89e121391ffee04070303ee3591f10bb
Rebecca Wang <wangmmqq@gmail.com>
2016-07-31 16:49:33 +0800

    implement is_member_of?(grou)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
   has_many :posts
 end
